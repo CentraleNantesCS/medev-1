040575594d4aa7504dc3f15b42864f9da76d65a5
Anas Mazouni <madadj4@gmail.com>
2020-11-19 11:02:56 +0100

    add: trycatch

--- src/main/java/edu/centralenantes/medev/tp1/Main.java
+++ src/main/java/edu/centralenantes/medev/tp1/Main.java
@@ -4,25 +4,32 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    //initialiser le plateau
+    // initialiser le plateau
     Plateau p = new Plateau();
-    
-    //Demander le nombre de joueurs
+    Scanner sc = new Scanner(System.in);
+    // Demander le nombre de joueurs
     System.out.println("Bienvenue sur Monopoly à Centrale Nantes");
     System.out.println("À combien de joueurs voulez-vous jouer ?");
-    Scanner sc = new Scanner(System.in);
-    String s = sc.next();
-    int nbJoueurs = Integer.parseInt(s);
-    
-    //Creation des joueurs en demandant leurs caracteristiques
-    for(int i=0; i<nbJoueurs; i++){
+
+    try {
+      String s = sc.next();
+      int nbJoueurs = Integer.parseInt(s);
+      for(int i=0; i<nbJoueurs; i++){
+      // Creation des joueurs en demandant leurs caracteristiques
       System.out.println("On commence par le joueur "+(i+1)+"\nEntrez votre nom, s'il vous plait.");
       String nom = sc.next();
       int fortune = 100000;
       int position = 0;
-      //ajouter le joueur a la liste dans plateau
       Joueur j = new Joueur(nom,fortune,position,p);
+      // ajouter le joueur a la liste dans plateau
       p.joueurs.add(j);
+    }
+    } catch (Exception e) {
+      System.err.println(e.getMessage());
+      if (sc != null) {
+        sc.close();
+      }
+      System.exit(1);
     }
   }
 }
