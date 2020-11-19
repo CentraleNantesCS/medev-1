0b15203bad6bed5a3065821e1ea19df98dce0336
tzeller97 <74487273+tzeller97@users.noreply.github.com>
2020-11-19 10:04:18 +0100

    Update Main.java

--- src/main/java/edu/centralenantes/medev/tp1/Main.java
+++ src/main/java/edu/centralenantes/medev/tp1/Main.java
@@ -1,7 +1,18 @@
 package edu.centralenantes.medev.tp1;
 
+import java.util.Scanner;
+
 public class Main {
   public static void main(String[] args) {
     System.out.println("Bienvenue sur Monopoly à Centrale Nantes");
+    System.out.println("À combien de joueurs voulez-vous jouer ?");
+    Scanner sc = new Scanner(System.in);
+    String s = sc.next();
+    int nbJoueurs = Integer.parseInt(s);
+    for(int i=0; i<nbJoueurs; i++){
+      System.out.println("On commence par le joueur "+(i+1)+"\nEntrez le nom de votre joueur, s'il vous plait.");
+      String s = sc.next();
+      
+    }
   }
 }
