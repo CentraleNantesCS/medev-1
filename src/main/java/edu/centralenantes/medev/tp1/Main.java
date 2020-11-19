753c81007a4ffd1384819b9304b61386bebf82c8
Anas Mazouni <madadj4@gmail.com>
2020-11-19 11:05:24 +0100

    fix: conflict

--- src/main/java/edu/centralenantes/medev/tp1/Main.java
+++ src/main/java/edu/centralenantes/medev/tp1/Main.java
@@ -15,28 +15,28 @@ public class Main {
     try {
       String s = sc.next();
       int nbJoueurs = Integer.parseInt(s);
-      for(int i=0; i<nbJoueurs; i++){
-      // Creation des joueurs en demandant leurs caracteristiques
-      System.out.println("On commence par le joueur "+(i+1)+"\nEntrez votre nom, s'il vous plait.");
-      String nom = sc.next();
-      int fortune = 100000;
-      int position = 0;
-      Joueur j = new Joueur(nom,fortune,position,p);
-      // ajouter le joueur a la liste dans plateau
-      p.joueurs.add(j);
-    
-    //Affichage
-    p.affiche();
-    
-    //Tour de Jeu
-        boolean jeuFini = false;
-        while(!jeuFini){
-            p.tourDeJeu();
-            p.affiche();
-            jeuFini = p.finDePartie();
-        }
-        //Jeu fini
-        System.out.println("Le jeu est fini.");
+      for (int i = 0; i < nbJoueurs; i++) {
+        // Creation des joueurs en demandant leurs caracteristiques
+        System.out.println("On commence par le joueur " + (i + 1) + "\nEntrez votre nom, s'il vous plait.");
+        String nom = sc.next();
+        int fortune = 100000;
+        int position = 0;
+        Joueur j = new Joueur(nom, fortune, position, p);
+        // ajouter le joueur a la liste dans plateau
+        p.joueurs.add(j);
+      }
+      //Affichage
+      p.affiche();
+
+      //Tour de Jeu
+      boolean jeuFini = false;
+      while(!jeuFini){
+          p.tourDeJeu();
+          p.affiche();
+          jeuFini = p.finDePartie();
+      }
+      //Jeu fini
+      System.out.println("Le jeu est fini.");
     } catch (Exception e) {
       System.err.println(e.getMessage());
       if (sc != null) {
