730e751d7ac935b24126adf91f8a75903685dd1d
tzeller97 <74487273+tzeller97@users.noreply.github.com>
2020-11-19 10:19:26 +0100

    Update Main.java

--- src/main/java/edu/centralenantes/medev/tp1/Main.java
+++ src/main/java/edu/centralenantes/medev/tp1/Main.java
@@ -4,15 +4,26 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
+    //initialiser le plateau
+    Plateau p = new Plateau();
+    
+    //Demander le nombre de joueurs
     System.out.println("Bienvenue sur Monopoly à Centrale Nantes");
     System.out.println("À combien de joueurs voulez-vous jouer ?");
     Scanner sc = new Scanner(System.in);
     String s = sc.next();
     int nbJoueurs = Integer.parseInt(s);
+    
+    //Creation des joueurs en demandant leurs caracteristiques
     for(int i=0; i<nbJoueurs; i++){
-      System.out.println("On commence par le joueur "+(i+1)+"\nEntrez le nom de votre joueur, s'il vous plait.");
-      String s = sc.next();
-      
+      System.out.println("On commence par le joueur "+(i+1)+"\nEntrez votre nom, s'il vous plait.");
+      String nom = sc.next();
+      //Quelle fortune au début du jeu ???
+      int fortune = 7500;
+      int position = 0;
+      //ajouter le joueur a la liste dans plateau
+      Joueur j = new Joueur(nom,fortune,position,p);
+      p.joueurs.add(j);
     }
   }
 }
